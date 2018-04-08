01536e482e4f5b62bcd208d3660b2832ef166f94
Jiun-Ting Ding <jiunting.d@gmail.com>
2018-04-08 12:24:19 +0800

    Initial Commit

---
+++ AlfaBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlfaBeacon
+//
+//  Created by kuasmis.wmc on 2018/4/8.
+//  Copyright © 2018 AlfaLoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
